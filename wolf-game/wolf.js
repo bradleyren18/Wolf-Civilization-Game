8c0934c369c6ac5c7249e2bb38f840277db35d7c
BradleyRen <148376421+BradleyRen@users.noreply.github.com>
2025-04-04 17:09:05 +0800

    All bugs fixed, v1.0 ready

--- wolf-game/wolf.js
+++ wolf-game/wolf.js
@@ -7,6 +7,7 @@ const meatCountText = document.getElementById("meatCount");
 const territoryCountText = document.getElementById("territoryCount");
 const wolvesCountText = document.getElementById("wolvesCount");
 const logText = document.getElementById("log");
+const latestEventText = document.getElementById("latestEvent");
 const timerText = document.getElementById("timer");
 const hunterAddBtn = document.getElementById("hunterAdd");
 const scoutAddBtn = document.getElementById("scoutAdd");
@@ -37,44 +38,12 @@ var bowEquipped = false;
 var crossbowEquipped = false;
 
 function daily(){
-    meatProductionRate = (hunters+helpers)*meatProducedPerHunter - totalWolves*meatConsumedPerWolf;
-    if (meat < 0 || meatProductionRate == 0){
-        alert("You lose :(");
-        clearInterval(interval);
-    }
-    if(meat >= 10 && bone >= 5){
-        lumberjackAddBtn.disabled = false;
-        minerAddBtn.disabled = false;
-    }else{
-        lumberjackAddBtn.disabled = true;
-        minerAddBtn.disabled = true;
-    }
-    if (meat >= 20 && bone >= 10){
-        hunterAddBtn.disabled = false;
-        scoutAddBtn.disabled = false;
-    }else{
-        hunterAddBtn.disabled = true;
-        scoutAddBtn.disabled = true;
-    }
-    if (meat >= 30 && bone >= 15){
-        helperAddBtn.disabled = false;
-    }else{
-        helperAddBtn.disabled = true;
-    }
-
-    if (territory >= 500){
-        alert("You win! :)");
-        clearInterval(interval);
-    }
-    display();
-    upgrades();
     meat -= totalWolves*meatConsumedPerWolf;
     bone -= totalWolves*meatConsumedPerWolf/2;
     currentTime++;
-    timerText.innerText = `Day ${currentTime}`;
     totalWolves = hunters+scouts+helpers+lumberjacks+miners;
 
-    var extraResources = (hunters+helpers)*meatProducedPerHunter/2;
+    const extraResources = (hunters+helpers)*meatProducedPerHunter/2;
     meat += extraResources*2;
     bone += extraResources;
     wood += lumberjacks*(woodProducedPerLumberjack+helpers);
@@ -114,22 +83,60 @@ function daily(){
     }
 }
 
+function perFrame(){
+    display();
+    upgrades();
+    meatProductionRate = (hunters+helpers)*meatProducedPerHunter - totalWolves*meatConsumedPerWolf;
+    lose = meatProductionRate == 0 && meat < 20;
+    if (meat < 0 || lose){
+        alert("You lose :(");
+        clearInterval(interval);
+    }
+    if(meat >= 10 && bone >= 5){
+        lumberjackAddBtn.disabled = false;
+        minerAddBtn.disabled = false;
+    }else{
+        lumberjackAddBtn.disabled = true;
+        minerAddBtn.disabled = true;
+    }
+    if (meat >= 20 && bone >= 10){
+        hunterAddBtn.disabled = false;
+        scoutAddBtn.disabled = false;
+    }else{
+        hunterAddBtn.disabled = true;
+        scoutAddBtn.disabled = true;
+    }
+    if (meat >= 30 && bone >= 15){
+        helperAddBtn.disabled = false;
+    }else{
+        helperAddBtn.disabled = true;
+    }
+
+    if (territory >= 500){
+        alert("You win! :)");
+        clearInterval(interval);
+    }
+    requestAnimationFrame(perFrame)
+}
 function events(){
     if (meat > 20 && territory > 1 && hunters > 1 && scouts > 1){
-        var chance = Math.floor(Math.random()*40);
+        const chance = Math.floor(Math.random()*40);
         if (chance == 10){
             meat -= 20;
             logText.innerText += "\n The meat storage hut was raided. You lose 20 meat :(";
+            latestEventText.innerText = "The meat storage hut was raided. You lose 20 meat :(";
         }
         else if (19 < chance < 23){
             territory -= 1;
             logText.innerText += "\n One of your territories revolted. You lose 1 territory :(";
+            latestEventText.innerText = "One of your territories revolted. You lose 1 territory :(";
         }
         else if (chance == 30){
             meat += 40;
             bone += 20;
             territory += 2;
             logText.innerText += "\n A neighboring pack joins yours. They gift you 40 meat, 20 bone and 2 territory.";
+            latestEventText.innerText = "A neighboring pack joins yours. They gift you 40 meat, 20 bone and 2 territory.";
         }
     }
 }
@@ -139,6 +146,7 @@ function upgrades(){
         if (knifeEquipped == false){
             knifeEquipped = true;
             logText.innerText += "\n All scouts have knives!";
+            latestEventText.innerText = "All scouts have knives!";
             stone -= 10;
             wood -= 20;
             bone -= 20;
@@ -148,6 +156,7 @@ function upgrades(){
         if (swordEquipped == false){
             swordEquipped = true;
             logText.innerText += "\n All scouts have swords!";
+            latestEventText.innerText = "All scouts have swords!";
             stone -= 20;
             wood -= 40;
             bone -= 40;
@@ -157,6 +166,7 @@ function upgrades(){
         if (spearEquipped == false){
             spearEquipped = true;
             logText.innerText += "\n All scouts have spears!";
+            latestEventText.innerText = "All scouts have spears!";
             stone -= 30;
             wood -= 60;
             bone -= 60;
@@ -166,6 +176,7 @@ function upgrades(){
         if (bowEquipped == false){
             bowEquipped = true;
             logText.innerText += "\n All scouts have bows!";
+            latestEventText.innerText = "All scouts have bows!";
             stone -= 40;
             wood -= 80;
             bone -= 80;
@@ -175,6 +186,7 @@ function upgrades(){
         if (crossbowEquipped == false){
             crossbowEquipped = true;
             logText.innerText += "\n All scouts have crossbows!";
+            latestEventText.innerText = "All scouts have crossbows!";
             stone -= 50;
             wood -= 50;
             bone -= 50;
@@ -183,6 +195,7 @@ function upgrades(){
 }
 
 function display(){
+    timerText.innerText = `Day ${currentTime}`;
     meatCountText.innerText = `Meat: ${meat} | Bone: ${bone} | Wood: ${wood} | Stone: ${stone}`;
     territoryCountText.innerText = `Territory: ${territory}`;
     wolvesCountText.innerText = `Hunters: ${hunters} | Scouts: ${scouts} | Helpers: ${helpers} | Lumberjacks: ${lumberjacks} | Miners: ${miners}`;
@@ -191,45 +204,41 @@ function display(){
 function update(){
     hunterAddBtn.onclick = ()=>{
         logText.innerText += "\n Hunter added.";
+        latestEventText.innerText = "Hunter added.";
         hunters += 1;
         meat -= 20;
         bone -= 10;
-        display();
-        upgrades();
     }
     scoutAddBtn.onclick = ()=>{
         logText.innerText += "\n Scout added.";
+        latestEventText.innerText = "Scout added.";
         scouts += 1;
         meat -= 20;
         bone -= 10;
-        display();
-        upgrades();
     }
     helperAddBtn.onclick = ()=>{
         logText.innerText += "\n Helper added.";
+        latestEventText.innerText = "Helper added.";
         helpers += 1;
         meat -= 30;
         bone -= 15;
-        display();
-        upgrades();
     }
     lumberjackAddBtn.onclick = ()=>{
         logText.innerText += "\n Lumberjack added.";
+        latestEventText.innerText = "Lumberjack added.";
         lumberjacks += 1;
         meat -= 10;
         bone -= 5;
-        display();
-        upgrades();
     }
     minerAddBtn.onclick = ()=>{
         logText.innerText += "\n Miner added.";
+        latestEventText.innerText = "Miner added.";
         miners += 1;
         meat -= 10;
         bone -= 5;
-        display();
-        upgrades();
     }
 }
 
 update();
+requestAnimationFrame(perFrame)
 var interval = setInterval(()=>{daily(); events()}, 1000);
